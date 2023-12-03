a776cd62c089c7ba0ccc951249ae073f082935f5
Chris Martin <shady@shadyproject.net>
2023-12-02 23:08:02 -0800

    day 2 part 2

--- python/day2.py
+++ python/day2.py
@@ -1,4 +1,6 @@
 from aocd import get_data
+from functools import reduce
+import operator
 
 
 def problem_one(num_red=12, num_green=13, num_blue=14):
@@ -34,7 +36,26 @@ def problem_one(num_red=12, num_green=13, num_blue=14):
 
 
 def problem_two():
-    pass
+    input = get_data(day=2, year=2023)
+
+    game_powers = []
+    for game in input.split("\n"):
+        needed = {"red": 0, "green": 0, "blue": 0}
+
+        (_, plays) = game.split(":")
+        # still don't really care about the individual sets of the game,
+        # so lets make it easier to parse
+        plays = plays.replace(";", ",")
+        for play in plays.split(","):
+            play = play.strip()
+            (count, color) = play.split(" ")
+            # simple addition here won't work, we need to check if the value is greater and then set it to that
+            if int(count) > needed[color]:
+                needed[color] = int(count)
+        power = needed["red"] * needed["green"] * needed["blue"]
+        game_powers.append(power)
+
+    return sum(game_powers)
 
 
 if __name__ == "__main__":
